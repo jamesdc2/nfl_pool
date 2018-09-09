9c1e15d002d4bddaaafa5c40dc7ace0b0ae2fb17
root <root@DESKTOP-0E2F3N3.localdomain>
2018-09-09 19:54:04 -0400

    Added last modified time

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask,render_template
+import os
+import time
 app = Flask(__name__)
 
 from pool import Leaderboard
 
-leaderboard = Leaderboard().from_csv('data/entries.csv','data/nfl_standings_week1_2.csv')
+standings_file = 'data/nfl_standings_week1_2.csv'
+leaderboard = Leaderboard().from_csv('data/entries.csv',standings_file)
 
 @app.route('/')
 def index():
+
+    statbuf = os.stat(standings_file)
+    last_mod = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(statbuf.st_mtime))
+
     entries = sorted(leaderboard.entries, key=lambda e: (-e.total_wins,e.total_losses,-e.diff))
-    return render_template('leaderboard.html',leaderboard=enumerate(entries))
+    return render_template('leaderboard.html',leaderboard=enumerate(entries),last_mod=last_mod)
 
 
 if __name__ == "__main__":
