fea97bedc0da383465cf2fa1f2874e3e350e2a76
root <root@DESKTOP-0E2F3N3.localdomain>
2018-09-08 20:50:19 -0400

    fixed refactor issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pool import Leaderboard
 
 leaderboard = Leaderboard().from_csv('data/entries.csv','data/nfl_standings_week1.csv')
 
-@application.route('/')
+@app.route('/')
 def index():
     entries = sorted(leaderboard.entries, key=lambda e: (-e.total_wins,e.total_losses,-e.diff))
     return render_template('leaderboard.html',leaderboard=enumerate(entries))
